5c0cee61ef4b27e8bc20d3d85eed8732754aaa0a
priyanka ramdas barate <92254687+baratepriyanka@users.noreply.github.com>
2024-05-19 10:46:21 +0545

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,1 @@
-var {Pool}=require('pg');
-const poolConnection = new Pool({
-    connectionString: process.env.POSTGRES_URL,
-  })
-  poolConnection.connect((err)=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log('Databases connected');
-})
-
-module.exports=poolConnection
+v
