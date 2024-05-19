07dbbaa1d18a90b84654ca495db1c4bc5e64c9d7
priyanka ramdas barate <92254687+baratepriyanka@users.noreply.github.com>
2024-05-19 10:47:32 +0545

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,1 @@
-const express = require('express')
-const loginController= require('../Controller/logincontroller');
-var router = express.Router();
-
-router.get('/all',loginController.getAll)
-router.get('/alldata',loginController.getAllData)
-
-module.exports=router
+c
